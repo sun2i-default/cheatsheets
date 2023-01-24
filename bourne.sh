0154dd7379bd863f752362b0e39e349a6f7f7869
Ben Basuni <benbasuni5067@gmail.com>
2023-01-24 16:16:22 -0600

    added git ssh cheat

--- bourne.sh
+++ bourne.sh
@@ -1,4 +1,6 @@
 # add .js extension to all files
 for f in * ; do if [ -f "$f" ] ; then t="${f%.*}" ; mv -i "$f" "$t".js ; fi ; done
 
+# add your ssh key to local agent (if you have trouble with github do this)
+ssh-add ~/.ssh/id_5067
 
